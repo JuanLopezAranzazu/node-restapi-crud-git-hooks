bb85b15ea7e51e9711f6d61bd5dc61cb103f528e
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2026-02-24 17:57:58 -0500

    add ESLint configuration and linting scripts

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.get("/", (_req, res) => {
+  res.send("Bievenido a mi API");
+});
+
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
